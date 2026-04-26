90cb1033b7e6c8f691388c92491db43460ca1af0
Solomon <solomonknudson@gmail.com>
2026-04-26 00:34:25 -0400

    turn concept macro off if concepts aren't available

--- include/operator/macros.hpp
+++ include/operator/macros.hpp
@@ -4,6 +4,11 @@
 #if defined(__cpp_concepts)
 #define OPERATOR_CREATE_REQUIRES(...) requires __VA_ARGS__
 #define OPERATOR_CREATE_TRAILING_RETURN(...)
+
+#define OPERATOR_CREATE_HAS_METHOD_CONCEPT(operation, method)                  \
+  template <typename Type, typename... Args>                                   \
+  concept Has##operation = OPERATOR_CREATE_REQUIRES(                           \
+      (Type type, Args&&... args) { type.method(args...); });
 #else
 #define OPERATOR_CREATE_REQUIRES(...)
 #define OPERATOR_CREATE_TRAILING_RETURN(...) ->__VA_ARGS__
@@ -14,8 +19,4 @@
   {                                                                            \
   };
 
-#define OPERATOR_CREATE_HAS_METHOD_CONCEPT(operation, method)                  \
-  template <typename Type, typename... Args>                                   \
-  concept Has##operation = OPERATOR_CREATE_REQUIRES(                           \
-      (Type type, Args&&... args) { type.method(args...); });
 #endif // OPERATOR_MACROS_HPP
