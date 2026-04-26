5a439b249070fe743881b7c3c8073908ab12d135
Solomon <solomonknudson@gmail.com>
2026-04-26 00:35:11 -0400

    expand namespaces and take in single value for methods that only accept single value

--- include/operator/deleters.hpp
+++ include/operator/deleters.hpp
@@ -5,61 +5,65 @@
 // STL
 #include <functional>
 
-namespace Operator::deleters
+namespace Operator
 {
-  template <typename Type, typename Value>
-  static decltype(auto)
-  scalar(Type& type, Value&& value)
+  namespace deleters
   {
-    return operation<tags::DeleteScalar>(type, std::forward<Value>(value));
-  }
-
-  template <typename Type, typename Value>
-  static decltype(auto)
-  array(Type& type, Value&& value)
-  {
-    return operation<tags::DeleteArray>(type, std::forward<Value>(value));
-  }
-
-  template <typename Type, typename Deleter>
-  static decltype(auto)
-  custom(Type& type, Deleter&& deleter)
-  {
-    return operation<tags::CustomDeleter>(type, std::forward<deleter>(deleter));
-  }
-} // namespace Operator::deleters
-
-namespace Operator::policies
-{
-  template <> struct Operator<tags::DeleteScalar>
-  {
-    template <typename T>
-    static void
-    operation(T* ptr)
-    {
-      delete ptr;
-    }
-  };
-
-  template <> struct Operator<tags::DeleteArray>
-  {
-    template <typename T>
-    static void
-    operation(T* ptr)
-    {
-      delete[] ptr;
-    }
-  };
-
-  template <> struct Operator<tags::CustomDeleter>
-  {
-    template <typename T, typename Deleter>
+    template <typename Type, typename Value>
     static decltype(auto)
-    operation(T&& ptr, Deleter&& deleter)
+    scalar(Type& type, Value&& value)
     {
-      return std::invoke(deleter, ptr);
+      return operation<tags::DeleteScalar>(type, std::forward<Value>(value));
     }
-  };
-} // namespace Operator::policies
+
+    template <typename Type, typename Value>
+    static decltype(auto)
+    array(Type& type, Value&& value)
+    {
+      return operation<tags::DeleteArray>(type, std::forward<Value>(value));
+    }
+
+    template <typename Type, typename Deleter>
+    static decltype(auto)
+    custom(Type& type, Deleter&& deleter)
+    {
+      return operation<tags::CustomDeleter>(type,
+                                            std::forward<deleter>(deleter));
+    }
+  } // namespace deleters
+
+  namespace policies
+  {
+    template <> struct Operator<tags::DeleteScalar>
+    {
+      template <typename T>
+      static void
+      operation(T* ptr)
+      {
+        delete ptr;
+      }
+    };
+
+    template <> struct Operator<tags::DeleteArray>
+    {
+      template <typename T>
+      static void
+      operation(T* ptr)
+      {
+        delete[] ptr;
+      }
+    };
+
+    template <> struct Operator<tags::CustomDeleter>
+    {
+      template <typename T, typename Deleter>
+      static decltype(auto)
+      operation(T&& ptr, Deleter&& deleter)
+      {
+        return std::invoke(deleter, ptr);
+      }
+    };
+  } // namespace policies
+} // namespace Operator
 #endif // OPERATOR_DELETERS_HPP
 
--- include/operator/inserters.hpp
+++ include/operator/inserters.hpp
@@ -3,108 +3,112 @@
 #include <operator/operation.hpp>
 #include <utility>
 
-namespace Operator::inserters
+namespace Operator
 {
-  template <typename Container, typename Value>
-  static decltype(auto)
-  push_front(Container& container, Value&& value)
+  namespace inserter
   {
-    return operation<tags::push_front>(container, std::forward<Value>(value));
-  }
-
-  template <typename Container, typename... Args>
-  static decltype(auto)
-  emplace_front(Container& container, Args&&... args)
-  {
-    return operation<tags::emplace_front>(container,
-                                          std::forward<Args>(args)...);
-  }
-
-  template <typename Container, typename Value>
-  static decltype(auto)
-  push_back(Container& container, Value&& value)
-  {
-    return operation<tags::push_back>(container, std::forward<Value>(value));
-  }
-
-  template <typename Container, typename... Args>
-  static decltype(auto)
-  emplace_back(Container& container, Args&&... args)
-  {
-    return operation<tags::emplace_back>(container,
-                                         std::forward<Args>(args)...);
-  }
-} // namespace Operator::inserters
-
-namespace Operator::policies
-{
-  template <typename Tag> struct Operator;
-
-  // Front insertions
-  template <> struct Operator<tags::push_front>
-  {
-    template <typename Container, typename... Args>
-    OPERATOR_CREATE_REQUIRES(concepts::HasPushFront<Container, Args...>)
-    static auto operation(Container& container, Args&&... args)
-        OPERATOR_CREATE_TRAILING_RETURN(
-            decltype(container.push_front(std::forward<Args>(args)...), void()))
+    template <typename Container, typename Value>
+    static decltype(auto)
+    push_front(Container& container, Value&& value)
     {
-      container.push_front(std::forward<Args>(args)...);
+      return operation<tags::push_front>(container, std::forward<Value>(value));
     }
-  };
 
-  template <> struct Operator<tags::emplace_front>
-  {
     template <typename Container, typename... Args>
-    OPERATOR_CREATE_REQUIRES(concepts::HasEmplaceFront<Container, Args...>)
-    static auto operation(Container& container, Args&&... args)
-        OPERATOR_CREATE_TRAILING_RETURN(
-            decltype(container.emplace_front(std::forward<Args>(args)...),
-                     void()))
+    static decltype(auto)
+    emplace_front(Container& container, Args&&... args)
     {
-      if constexpr (sizeof...(Args) == 0)
-      {
-        container.emplace_front();
-      }
-      else
-      {
-        (container.emplace_front(std::forward<Args>(args)), ...);
-      }
+      return operation<tags::emplace_front>(container,
+                                            std::forward<Args>(args)...);
     }
-  };
 
-  // Back insertions
-  template <> struct Operator<tags::push_back>
-  {
-    template <typename Container, typename... Args>
-    OPERATOR_CREATE_REQUIRES(concepts::HasPushBack<Container, Args...>)
-    static auto operation(Container& container, Args&&... args)
-        OPERATOR_CREATE_TRAILING_RETURN(
-            decltype(container.push_back(std::forward<Args>(args)...), void()))
+    template <typename Container, typename Value>
+    static decltype(auto)
+    push_back(Container& container, Value&& value)
     {
-      container.push_back(std::forward<Args>(args)...);
+      return operation<tags::push_back>(container, std::forward<Value>(value));
     }
-  };
 
-  template <> struct Operator<tags::emplace_back>
-  {
     template <typename Container, typename... Args>
-    OPERATOR_CREATE_REQUIRES(concepts::HasEmplaceBack<Container, Args...>)
-    static auto operation(Container& container, Args&&... args)
-        OPERATOR_CREATE_TRAILING_RETURN(
-            decltype(container.emplace_back(std::forward<Args>(args)...),
-                     void()))
+    static decltype(auto)
+    emplace_back(Container& container, Args&&... args)
     {
-      if constexpr (sizeof...(Args) == 0)
-      {
-        container.emplace_back();
-      }
-      else
-      {
-        (container.emplace_back(std::forward<Args>(args)), ...);
-      }
+      return operation<tags::emplace_back>(container,
+                                           std::forward<Args>(args)...);
     }
-  };
-} // namespace Operator::policies
+  } // namespace inserter
+
+  namespace policies
+  {
+    template <typename Tag> struct Operator;
+
+    // Front insertions
+    template <> struct Operator<tags::push_front>
+    {
+      template <typename Container, typename Value>
+      OPERATOR_CREATE_REQUIRES(concepts::HasPushFront<Container, Args...>)
+      static auto operation(Container& container, Value&& value)
+          OPERATOR_CREATE_TRAILING_RETURN(
+              decltype(container.push_front(std::forward<Value>(value)),
+                       void()))
+      {
+        container.push_front(std::forward<Value>(value));
+      }
+    };
+
+    template <> struct Operator<tags::emplace_front>
+    {
+      template <typename Container, typename... Args>
+      OPERATOR_CREATE_REQUIRES(concepts::HasEmplaceFront<Container, Args...>)
+      static auto operation(Container& container, Args&&... args)
+          OPERATOR_CREATE_TRAILING_RETURN(
+              decltype(container.emplace_front(std::forward<Args>(args)...),
+                       void()))
+      {
+        if constexpr (sizeof...(Args) == 0)
+        {
+          container.emplace_front();
+        }
+        else
+        {
+          (container.emplace_front(std::forward<Args>(args)), ...);
+        }
+      }
+    };
+
+    // Back insertions
+    template <> struct Operator<tags::push_back>
+    {
+      template <typename Container, typename Value>
+      OPERATOR_CREATE_REQUIRES(concepts::HasPushBack<Container, Args...>)
+      static auto operation(Container& container, Value&& value)
+          OPERATOR_CREATE_TRAILING_RETURN(
+              decltype(container.push_back(std::forward<Value>(value)), void()))
+      {
+        container.push_back(std::forward<Value>(value));
+      }
+    };
+
+    template <> struct Operator<tags::emplace_back>
+    {
+      template <typename Container, typename... Args>
+      OPERATOR_CREATE_REQUIRES(concepts::HasEmplaceBack<Container, Args...>)
+      static auto operation(Container& container, Args&&... args)
+          OPERATOR_CREATE_TRAILING_RETURN(
+              decltype(container.emplace_back(std::forward<Args>(args)...),
+                       void()))
+      {
+        if constexpr (sizeof...(Args) == 0)
+        {
+          container.emplace_back();
+        }
+        else
+        {
+          (container.emplace_back(std::forward<Args>(args)), ...);
+        }
+      }
+    };
+  } // namespace policies
+} // namespace Operator
 #endif // OPERATOR_INSERTERS_HPP
 
