d844562d7923557f59ff7a0ddabdea1577bd0805
Solomon <solomonknudson@gmail.com>
2026-04-25 22:16:19 -0400

    add remove_pointer alias

--- include/operator/util.hpp
+++ include/operator/util.hpp
@@ -1,29 +1,32 @@
 #ifndef OPERATOR_UTIL_HPP
 #define OPERATOR_UTIL_HPP
-#include <functional>
 #include <operator/macros.hpp>
 
-// 3rd party
+// STL
+#include <functional>
 #include <iostream>
+#include <type_traits>
 
 namespace Operator
 {
   namespace util
   {
+    template <typename T> using remove_pointer = std::remove_pointer_t<T>;
+
     template <typename Container,
               typename Printer =
                   std::function<void(typename Container::value_type&& value)>>
-    OPERATOR_CONCEPT(requires(Container& container) {
+    OPERATOR_CREATE_REQUIRES(requires(Container& container) {
       container.cbegin();
       container.cend();
     })
     static auto display(
         const Container& container,
-        const Printer& print = [](typename Container::value_type&& value)
+        Printer&& print = [](typename Container::value_type&& value)
         { std::cout << value << " "; })
-        OPERATOR_TRAILING_RETURN(decltype(container.cbegin(),
-                                          container.cend(),
-                                          void()))
+        OPERATOR_CREATE_TRAILING_RETURN(decltype(container.cbegin(),
+                                                 container.cend(),
+                                                 void()))
     {
       for (auto it{container.cbegin()}, end{container.cend()}; it != end; ++it)
       {
