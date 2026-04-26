125fac6d5f8415197993c48044263dd4231330e0
Solomon <solomonknudson@gmail.com>
2026-04-26 18:12:54 -0400

    implement NoOp policy for fun

--- include/operator/policies.hpp
+++ include/operator/policies.hpp
@@ -1,10 +1,20 @@
 #ifndef OPERATOR_POLICIES_HPP
 #define OPERATOR_POLICIES_HPP
+#include <operator/tags.hpp>
 
 namespace Operator::policies
 {
   template <typename Tag> struct Operator
   {
+  };
+
+  template <> struct Operator<tags::NoOp>
+  {
+    template <typename... Args>
+    static void
+    operation(Args&&...)
+    {
+    }
   };
 } // namespace Operator::policies
 #endif // OPERATOR_POLICIES_HPP
