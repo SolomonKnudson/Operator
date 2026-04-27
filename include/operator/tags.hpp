04276c517a476fe31a5793e0f8eea825a068b9b9
Solomon <solomonknudson@gmail.com>
2026-04-27 18:22:39 -0400

    move lib defined tags back into tags.hpp

--- include/operator/tags.hpp
+++ include/operator/tags.hpp
@@ -10,7 +10,25 @@ namespace Operator::tags
    *  };
    *  OPERATOR_CREATE_TAG(name) example output
    */
+  // Insertions
+  // Front Insertions
+  OPERATOR_CREATE_TAG(push_front);
+  OPERATOR_CREATE_TAG(emplace_front);
+  //END Front insertions
+
+  // Back Insertions
+  OPERATOR_CREATE_TAG(push_back);
+  OPERATOR_CREATE_TAG(emplace_back);
+  //END Back Insertions
+  //END Insertions
+
+  // Deleters
+  OPERATOR_CREATE_TAG(DeleteScalar)
+  OPERATOR_CREATE_TAG(DeleteArray)
+  OPERATOR_CREATE_TAG(CustomDeleter)
+
   // Misc
   OPERATOR_CREATE_TAG(NoOp)
+  //END Misc
 } // namespace Operator::tags
 #endif // OPERATOR_TAGS_HPP
