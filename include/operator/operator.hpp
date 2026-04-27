18aec598250f4cbf5952c185a568ebf1844f5dd5
Solomon <solomonknudson@gmail.com>
2026-04-27 18:30:58 -0400

    remove concepts.hpp and macros.hpp from umbrella header
    conceps.hpp and macros.hpp are 'internal' lib headers only and should not be used directly. They are subject to change at any time.

--- include/operator/operator.hpp
+++ include/operator/operator.hpp
@@ -1,9 +1,7 @@
 #ifndef OPERATOR_HPP
 #define OPERATOR_HPP
-#include <operator/concepts.hpp>
 #include <operator/deleters.hpp>
 #include <operator/inserters.hpp>
-#include <operator/macros.hpp>
 #include <operator/operation.hpp>
 #include <operator/policies.hpp>
 #include <operator/tags.hpp>
