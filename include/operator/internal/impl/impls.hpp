aeb7f0dac43f8d692bde88531d212122710bd4ae
Solomon <solomonknudson@gmail.com>
2026-07-24 10:26:07 -0400

    formatting

--- include/operator/internal/impl/impls.hpp
+++ include/operator/internal/impl/impls.hpp
@@ -2,6 +2,5 @@
 #define OPERATOR_INTERNAL_IMPLS
 #include <operator/internal/impl/inserter.hpp>
 #include <operator/internal/impl/invoke.hpp>
-
 #include <operator/internal/impl/miscellaneous.hpp>
 #endif // OPERATOR_INTERNAL_IMPLS
--- include/operator/internal/impl/miscellaneous.hpp
+++ include/operator/internal/impl/miscellaneous.hpp
@@ -6,22 +6,6 @@
 
 namespace Operator
 {
-  template <> struct Impl<builtin::DisplayContainer>
-  {
-    template <typename Container, typename Printer>
-    static void
-    invoke(Container&& container,
-           Printer&& printer,
-           const std::string& prefix = "",
-           const std::string& suffix = "\n")
-    {
-      util::display_container(std::forward<Container>(container),
-                              std::forward<Printer>(printer),
-                              prefix,
-                              suffix);
-    }
-  };
-
   template <> struct Impl<builtin::NoOp>
   {
     template <typename... Args>
